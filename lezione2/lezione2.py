ca0d011b98667cffc1900f3cbef6dc57b1f330c3
danielinoo <daniele.pietropaolo1234@gmail.com>
2024-04-18 13:33:59 +0200

    risolti esercizi 3,4,5 (2-5,2-8,3-1)

--- lezione2/lezione2.py
+++ lezione2/lezione2.py
@@ -34,5 +34,49 @@ print("-----------------------")
 # METODO COMPRESSO   ------>   print(f" {name}, {name.upper()}, {name.lower()}")
 
 
+"""----------------------------------------------------------------------"""
+
+#esercizio 2-5. Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks: Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+
+#variabili
+
+nome : str = "Pirandello"
+testo : str = "Imparerai a tue spese che nel lungo tragitto della vita incontrerai tante maschere e pochi volti."
+
+print( f"{nome} una volta disse:\n\" {testo}\" ")
+print("-----------------------")
+
+
+"""----------------------------------------------------------------------"""
+
+#esercizio 2-8. File Extensions: Python has a removesuffix() method that works exactly like removeprefix(). Assign the value 'python_notes.txt' to a variable called filename. Then use the removesuffix() method to display the filename without the file extension, like some file browsers do.
+
+
+file_name : str = "python-notes.txt"
+
+
+#rimuove .txt dalla stringa
+file_name =file_name.removesuffix(".txt")
+
+print(file_name)
+print("-----------------------")
+
+
+"""----------------------------------------------------------------------"""
+
+#esercizio 3-1. Names: Store the names of a few of your friends in a list called names. Print each person’s name by accessing each element in the list, one at a time.
+
+nomi : list = ["marco","luigi","leonardo","valerio"]
+
+for i in range (len(nomi)):
+
+    print(nomi[i])
+
+
+print("-----------------------")
+
+
+"""----------------------------------------------------------------------"""
 
 
