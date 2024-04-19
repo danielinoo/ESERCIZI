e2a7f790a49db64968d8ad3f9afce7d2d8d5ef20
danielinoo <daniele.pietropaolo1234@gmail.com>
2024-04-19 09:42:24 +0200

    risolto esercizio 9 (3-5)

--- lezione2/lezione2.py
+++ lezione2/lezione2.py
@@ -97,10 +97,10 @@ print("-----------------------")
 
 nomi : list = ["Rolls-Royce ","Lamborghini ","Ferrari" ,"Aston Martin ","Bentley"]
 
-mes : str = " ma sono povero "
+mes : str = " vorrei guidare una"
 for i in range (len(nomi)):
 
-    print(f"vorrei una {nomi[i]}{mes}")
+    print(f"{mes} {nomi[i]}")
 
 print("-----------------------")
 
@@ -113,12 +113,35 @@ nomi : list = ["snoop dog ","Miriam Leone ","Ludovica Pagani "]
 
 mes : str = "vieni a cena "
 for i in range (len(nomi)):
+    
+    print(f"ciao {nomi[i]}{mes}")
 
+"""----------------------------------------------------------------------"""
+
+
+#esercizio 3-5. Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
+ #Start with your program from Exercise 3-4. Add a print() call at the end of your program, stating the name of the guest who can’t make it.Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
+#Print a second set of invitation messages, one for each person who is still in your list.
+
+print("\n")
+print("snoop dog non può venire")
+print("\n")
+
+nomi[0] = "Angelina Mango "
+
+for i in range (len(nomi)):
+    
     print(f"ciao {nomi[i]}{mes}")
 
 print("-----------------------")
 
-"""----------------------------------------------------------------------"""
+
+
+
+
+
+
+
 
 
 
