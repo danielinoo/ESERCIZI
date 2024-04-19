1294184c6ffd7354c20700fa824b9fd7ed8c0993
danielinoo <daniele.pietropaolo1234@gmail.com>
2024-04-19 13:30:02 +0200

    risolto esercizio 10 (3-6)

--- lezione2/lezione2.py
+++ lezione2/lezione2.py
@@ -109,9 +109,11 @@ print("-----------------------")
 
 #esercizio 3-4. Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
 
+#creazione lista
 nomi : list = ["snoop dog ","Miriam Leone ","Ludovica Pagani "]
 
-mes : set = {"vieni a cena "}
+#creazione messaggio
+mes : set = {" vieni a cena "}
 for i in range (len(nomi)):
     
     print(f"ciao {nomi[i]}{mes}")
@@ -127,6 +129,7 @@ print("\n")
 print("snoop dog non può venire")
 print("\n")
 
+#inserimento nuovo nome
 nomi[0] = "Angelina Mango "
 
 for i in range (len(nomi)):
@@ -134,11 +137,27 @@ for i in range (len(nomi)):
     print(f"ciao {nomi[i]}{mes}")
 
 
+#esercizio 3-6. More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests to invite to dinner.
+#• Start with your program from Exercise 3-4 or 3-5. Add a print() call to the end of your program, informing people that you found a bigger table.
+#• Use insert() to add one new guest to the beginning of your list.
+#• Use insert() to add one new guest to the middle of your list.
+#• Use append() to add one new guest to the end of your list.
+#• Print a new set of invitation messages, one for each person in your list.
+
+
 print("\n")
 print("trovato un tavolo più grande ")
 print("\n")
 
+#inserimento altri nomi
+nomi.insert(0,"The rock")
+nomi.insert(4,"gigi proietti")
+nomi.append("max verstappen")
 
+
+for i in range (len(nomi)):
+    
+    print(f"ciao {nomi[i]}{mes}")
 
 print("-----------------------")
 
