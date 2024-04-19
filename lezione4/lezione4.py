348c6c12a958da693bf5d0c4a23dcbd6b1951209
danielinoo <daniele.pietropaolo1234@gmail.com>
2024-04-19 12:54:47 +0200

    esercizi funzioni 2

--- lezione4/lezione4.py
+++ lezione4/lezione4.py
@@ -12,8 +12,31 @@ def subtrack(a : int,b : int) -> int:
 
 print(subtrack(67,4))
 print("-----------------------------")
-print("\n")
+
 
 
 """------------------------------------------------"""
 
+
+#ESERCIZIO 2
+
+def cv(n : int) -> int:
+
+    if n == 5:
+        print("il numero è uguale a 5")
+    elif n < 5:
+        print("il numero è minore di cinque")
+
+    else: 
+        print("il numero è maggiore di 5")
+
+cv(2)
+
+
+
+
+print("-----------------------------")
+
+
+
+"""------------------------------------------------"""
