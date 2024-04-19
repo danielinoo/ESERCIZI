c9b94d5dc699e5387de38bdbfe88194fb5c2f1a7
danielinoo <daniele.pietropaolo1234@gmail.com>
2024-04-19 11:54:58 +0200

    esercizi funzioni 1

---
+++ lezione4/lezione4.py
@@ -0,0 +1,15 @@
+#Daniele Pietropaoo
+#19/04/24
+
+#ESERCIZIO 1
+
+
+#funzione di sottrazione
+def subtrack(a : int,b : int) -> int:
+
+
+    return a - b
+
+print(subtrack(15,5))
+print("-----------------------------")
+print("\n")
