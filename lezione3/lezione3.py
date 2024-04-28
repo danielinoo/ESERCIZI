ae9982bd7a8a79c539cb1414f72235b3c04bba92
danielinoo <daniele.pietropaolo1234@gmail.com>
2024-04-28 15:45:38 +0200

    lezione3 esercizi fino al 5-4( riverdere 4-12,4-15,5-2)

--- lezione3/lezione3.py
+++ lezione3/lezione3.py
@@ -336,10 +336,63 @@ print("\n")
 #• Test whether an item is not in a list
 
 
+
+
+
+
+
+
+
+
 print("\n")
 print("|-----------------------------------------------------------------------------|")
 """-----------------------------------------------------------------------------------------------------------------------------------"""
 print("\n") 
+
+#esercizio 5-3. Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+#• Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+#• Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
+
+giocatore1 : int = 0
+
+alien_color = "verde"
+
+if alien_color == "verde":
+
+    giocatore1 = giocatore1 + 5
+    print("il giocatore1 ha appena ottenuto 5 punti")
+
+if alien_color == "rosso":
+
+    pass
+
+
+print("\n")
+print("|-----------------------------------------------------------------------------|")
+"""-----------------------------------------------------------------------------------------------------------------------------------"""
+print("\n") 
+
+#esercizio 5-4. Alien Colors #2: Choose a color for an alien as you did in Exercise 5-3, and write an if-else chain.
+#• If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
+#• If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+#• Write one version of this program that runs the if block and another that runs the else block.
+
+alien_color2 : str = "rosso"
+
+if alien_color2 == "verde":
+    giocatore1 = giocatore1 + 5
+    print("complimenti hai guadagnato 5 punti per aver sparato all' alieno")
+
+else:
+
+    giocatore1 = giocatore1 + 5
+    print("complimenti hai guadagnato 10 punti")
+
+print("\n")
+print("|-----------------------------------------------------------------------------|")
+"""-----------------------------------------------------------------------------------------------------------------------------------"""
+print("\n") 
+
 
 
 
