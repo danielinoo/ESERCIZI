0c4e23488724272e388bc1de5812e644291dca6d
danielinoo <daniele.pietropaolo1234@gmail.com>
2024-04-29 17:13:05 +0200

    LEZIONE4: esercizi risolti fino al 8-7 (rivedere l'8-7)

--- lezione4/lezione4.py
+++ lezione4/lezione4.py
@@ -98,6 +98,23 @@ describe_city("Parigi","Francia")
 describe_city("Londra", "Regno unito")
 
 
+print("\n|--------------------------------------|\n")
+"""---------------------------------------------------------------------------------------------------------------------------"""
+
+#esercizio 8-7. Album: Write a function called make_album() that builds a dictionary describing a music album. The function should take in an artist name and an album title, and it should return a dictionary containing these two pieces of information. Use the function to make three dictionaries representing different albums. Print each return value to show that the  dictionaries are storing the album information correctly. Use None to add an optional parameter to make_album() that allows you to store the number of songs on an album. If the calling line includes a value for the number of songs, add that value to the album’s dictionary. Make at least one new function call that includes the number of songs on an album.
+
+def  make_album(artista : str,nomA : str,numero_brani = None)-> dict:
+    
+    album = {"artista": artista,"album": nomA}
+
+    if numero_brani:
+        album["numero brani"] = numero_brani
+
+        return album
+    
+print(make_album("Pink Floyd","the wall"))
+print("-----")
+print(make_album("Bob marley","Love songs",8))
 
 
 
