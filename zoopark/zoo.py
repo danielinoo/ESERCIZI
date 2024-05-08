abfee7bbd67d90c1a556368df80bc86afd0edb86
danielinoo <daniele.pietropaolo1234@gmail.com>
2024-05-08 18:54:28 +0200

    zoopark sistema classi

---
+++ zoopark/zoo.py
@@ -0,0 +1,47 @@
+#daniele pietropaolo
+#5/05/24
+
+class zoo:
+    def __init__(self,recinto : fence,):
+
+        self.rec = recinto
+        self.gu = g
+
+
+
+
+#-------------------------------#
+
+class zoo_keepers:
+
+    def __init__(self,n : str,c : str,idi : str):
+
+        self.nome = n
+        self.cognome = c
+        self.id = idi
+
+#-------------------------------#
+
+
+class animal:
+
+    def __init__(self,n : str,s: str,eta : int,alt : float,larg : float,habitat : str):
+
+
+        self.name_a = n
+        self.spieces = s
+        self.age_s = eta
+        self.height = alt
+        self.width = larg
+        self.prefer_habitat = habitat
+
+
+
+
+
+
+#---------------------------#
+
+
+class fence:
+    def __init__(self,):
