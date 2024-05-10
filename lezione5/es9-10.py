5895bc9a1fa4c453794393330cfece568244890a
danielinoo <daniele.pietropaolo1234@gmail.com>
2024-05-10 19:00:56 +0200

    LEZIONE5: rivedere 9-10

---
+++ lezione5/es9-10.py
@@ -0,0 +1,9 @@
+#Daniele pietropaolo
+
+#esercizio 9-10. Imported Restaurant: Using your latest Restaurant class, store it in a module. Make a separate file that imports Restaurant. Make a Restaurant instance, and call one of Restaurant’s methods to show that the import statement is working properly.
+
+from lezione5 import Restaurant
+
+r = Restaurant(ristorante= "Da Baffo",cucina="romana",num= 2)
+
+print(Restaurant)
