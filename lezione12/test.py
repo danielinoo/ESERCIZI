5a4cb81de363b8a105364afe1ec25afcceecc458
danielinoo <daniele.pietropaolo1234@gmail.com>
2024-05-31 12:34:24 +0200

    esempi di assert

---
+++ lezione12/test.py
@@ -0,0 +1,30 @@
+# Daniele Pietropaolo
+
+# i : int = 0
+# test_value = 1
+
+
+# #assert --> controlla se quello scritto affianco è true. se è false te da n errore e te blocca il programma(non bono mejo er debugger) lo scopo di assert è fare il TEST
+# assert i == 0
+
+
+
+
+# #da dei messaggi personalizzati quando da errore
+
+# assert i == test_value,f"ERRORE LA VARIABILE DEVE ESSE {test_value} E NON {i}"
+
+
+
+def sum(a ,b):
+
+    return 0
+
+
+result = sum(5,2)
+
+test_value = 7
+
+
+assert result == test_value,f"errore, il risultatpo deve esse {test_value} e non {result}"
+
