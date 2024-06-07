cb57915a1615b3490a0a2824fe069e2d11d3f0d7
danielinoo <daniele.pietropaolo1234@gmail.com>
2024-06-07 13:35:10 +0200

    lezione16: verificare esercizio 2

--- lezione16/lezione16.py
+++ lezione16/lezione16.py
@@ -1,169 +1,160 @@
-#Daniele Pietropaolo
+#Pietropaolo Daniele
 
 
-# Obiettivo:
-# Implementare una classe Media che rappresenti un media generico (ad esempio, un film o un libro) e una classe derivata Film che rappresenti specificamente un film. Gli studenti dovranno anche creare oggetti di queste classi, aggiungere valutazioni e visualizzare le recensioni.
+# Vogliamo gestire un contatore che può essere incrementato, decrementato, resettato e visualizzato. La classe offre un modo semplice per tenere traccia di un conteggio che non può diventare negativo.
 
-# Specifiche della Classe Media:
- 
+# Classe Contatore
 # Attributi:
-# - title (stringa): Il titolo del media.
-# - reviews (lista di interi): Una lista di valutazioni del media, con voti compresi tra 1 e 5.
+# - conteggio: un intero che conserva il valore del conteggio, inizializzato a 0.
 
 # Metodi:
-# - set_title(self, title): Imposta il titolo del media.
-# - get_title(self): Restituisce il titolo del media.
-# - aggiungiValutazione(self, voto): Aggiunge una valutazione alla lista delle recensioni se è valida (tra 1 e 5).
-# - getMedia(self): Calcola e restituisce la media delle valutazioni.
-# - getRate(self): Restituisce una stringa che descrive il giudizio medio del media basato sulla media delle valutazioni.
-# - ratePercentage(self, voto): Calcola e restituisce la percentuale di un voto specifico nelle recensioni.
-# - recensione(self): Mostra un riassunto delle recensioni e delle valutazioni del media, stampando il titolo, il voto medio, il giudizio e le percentuali di ciascun voto. Esempio di riassunto:
- 
-# Titolo del Film: The Shawshank Redemption
-# Voto Medio: 3.80
-# Giudizio: Bello
-# Terribile: 10.00%
-# Brutto: 10.00%
-# Normale: 10.00%
-# Bello: 30.00%
-# Grandioso: 40.00%
-
-# Si verifichi il funzionamento scrivendo un codice che crei almeno due oggetti di tipo Film, aggiunga a ognuno dei due almeno dieci valutazioni e richiami il metodo recensione().
+# - __init__(): Inizializza l'attributo conteggio a 0.
+# - setZero(): Imposta il conteggio a 0.
+# - add1(): Incrementa il conteggio di 1.
+# - sub1(): Decrementa il conteggio di 1, ma non permette che il conteggio diventi negativo. Se il conteggio è già 0, stampa un messaggio di errore.
+# - get(): Restituisce il valore corrente del conteggio.
+# - mostra(): Stampa a schermo il valore corrente del conteggio.
 
 
 
 
 
-class Media:
 
-    def __init__(self,title : str,reviews :list[int]) -> None:
-        self.title = title
-        self.reviews = reviews
-        self.m : float = 0
+class Contatore:
+    def __init__(self) -> None:
+        self.conteggio = 0
 
 
-    def set_title(self, title):
-            
-            self.title = title
-
-    def get_title(self):
-            return f"\nTitolo: {self.title}\n"
+    def setZero(self):
+        
+        self.conteggio = 0
 
 
-    def aggiungiValutazione(self, voto):
-            
+    def add1(self): 
+        
+        self.conteggio += 1
 
-        if voto <= 5:
-            
-            self.reviews.append(voto)
+
+    def sub1(self):
+
+        if self.conteggio > 0:
+            self.conteggio -= 1
 
         else:
-            
-            pass
+            print("Non è possibile eseguire la sottrazione")
+
+    def get(self):
+        return self.conteggio
+
+    def mostra(self):
+        print( f"Conteggio attuale: {self.conteggio}")
 
 
-    def getMedia(self):
+    
+##############################################################
 
-        self.m : float =sum(self.reviews) / len(self.reviews)
+# Sviluppa un sistema per la gestione delle ricette in Python che permetta agli utenti di creare, modificare, e cercare ricette basate sugli ingredienti. Il sistema dovrà essere capace di gestire una collezione di ricette e i loro ingredienti.
 
-        return self.m
+# Classe:
+# - RecipeManager:
+#     Gestisce tutte le operazioni legate alle ricette.
+
+#     Metodi:
+#     - create_recipe(name, ingredients): Crea una nuova ricetta con il nome specificato e una lista di ingredienti. Restituisce un dizionario con la ricetta appena creata o un messaggio di errore se la ricetta esiste già.
+
+#     - add_ingredient(recipe_name, ingredient): Aggiunge un ingrediente alla ricetta specificata. Restituisce la ricetta aggiornata o un messaggio di errore se l'ingrediente esiste già o la ricetta non esiste.
+
+#     - remove_ingredient(recipe_name, ingredient): Rimuove un ingrediente dalla ricetta specificata. Restituisce la ricetta aggiornata o un messaggio di errore se l'ingrediente non è presente o la ricetta non esiste.
+
+#     - update_ingredient(recipe_name, old_ingredient, new_ingredient): Sostituisce un ingrediente con un altro nella ricetta specificata. Restituisce la ricetta aggiornata o un messaggio di errore se l'ingrediente non è presente o la ricetta non esiste.
+
+#     - list_recipes(): Elenca tutte le ricette esistenti.
+
+#     - list_ingredients(recipe_name): Mostra gli ingredienti di una specifica ricetta. Restituisce un elenco di ingredienti o un messaggio di errore se la ricetta non esiste.
+
+#     - search_recipe_by_ingredient(ingredient): Trova e restituisce tutte le ricette che contengono un determinato ingrediente. Restituisce un elenco di ricette o un messaggio di errore se nessuna ricetta contiene l'ingrediente.
 
 
-    def getRate(self):
-         
-        if self.m <= 1.5:
-             
-             return "TERRIBILE"
+
+
+class RecipeManager:
+
+    def __init__(self) -> None:
+        self.collezione : dict[str: list[str]] = {}
+        self.l : list[str] = []
+
+
+    def create_recipe(self,name, ingredients):
+
+        if name not in self.collezione:
+
+            self.collezione[name] = self.l.append(ingredients)
+
+        else:
+
+            print("errore")
+
+
+
+    def add_ingredient(self,recipe_name, ingredient):
+
+        if recipe_name in self.collezione:
+            self.collezione[recipe_name] = self.l.append(ingredient)
+
+
+    def remove_ingredient(self,recipe_name, ingredient):
+
+        if recipe_name in self.collezione:
+            self.collezione[recipe_name] = self.l.remove(ingredient)
+
+
+    def update_ingredient(self,recipe_name, old_ingredient, new_ingredient):
+
         
-        elif self.m <= 2.5:
-             
-             return "BRUTTO"
+        if recipe_name in self.collezione:
 
-        elif self.m <=3.5:
+            if old_ingredient in self.collezione[recipe_name]:
 
-
-            return "BELLO(ma se potevano impegna de più)"  
-
-        elif self.m <= 4.5:
-             
-             return "BELLO"
+                self.collezione[recipe_name] = self.l.remove(old_ingredient)
+                self.collezione[recipe_name] = self.l.append(new_ingredient)
         
-        elif self.m > 5:
-             
-            return"GRANDIOSO"
-
-                     
-    
-
-    def ratePercentage(self, voto):
+            else:
+                return "l ingrediente non esiste"
         
-        p : int =self.reviews.count(voto)
-
-        percentuale = (len(self.reviews) * p) / 100
-
-        return f"{percentuale}%"
-
-
-
-    def recensione(self):   
         
-        print(f"\ntitolo del film: {self.title}\n")
-        print(f"\nvoto medio:{self.m}\n")
-
-        print(f"\ngiudizio:{self.getRate()}\n")
-        print(f"\nTerribile: {self.ratePercentage(1)}\n")
-        print(f"\nBrutto:{self.ratePercentage(2)}\n")
-        print(f"\nNormale:{self.ratePercentage(3)}\n")
-        print(f"\nBello:{self.ratePercentage(4)}\n")
-        print(f"\nGrandioso:{self.ratePercentage(5)}\n")
+        else:
+            return "la ricetta non esiste"
 
 
-class Film(Media):
-    def __init__(self, title: str, reviews: list[int]) -> None:
-        super().__init__(title, reviews)
+    def list_recipes(self): 
+        return f"{self.collezione.keys()}"
+
+    def list_ingredients(self,recipe_name):
+
+        if recipe_name in self.collezione:
+            return f"{self.collezione.values(recipe_name)}"
+        
+        else:
+            return "la ricetta non esiste"
+
+    def search_recipe_by_ingredient(self,ingredient): 
+
+        l1 : list[str] = []
+        
+        for k,v in self.collezione.items():
+
+            if ingredient in k:
+
+                l1.append(k)
 
 
-    def set_title(self, title):
-        return super().set_title(title)
-    
-    def get_title(self):
-        return super().get_title()
-    
-    def aggiungiValutazione(self, voto):
-         return super().aggiungiValutazione(voto)
-    
-    def getMedia(self):
-         return super().getMedia()
-    
-    def getRate(self):
-         return super().getRate()
-    
-    def ratePercentage(self, voto):
-         return super().ratePercentage(voto)
-    
+        if len(l1) != 0:
+
+            return l1
+        
+        else:
+
+            return "errore la ricetta non esiste"
 
 
-
-film_1 = Film("deadpool","non so")
-
-film_1.set_title("deadpool")
-    
-film_1.aggiungiValutazione(3)
-film_1.aggiungiValutazione(3)
-film_1.aggiungiValutazione(3)
-film_1.aggiungiValutazione(3)
-film_1.aggiungiValutazione(3)
-film_1.aggiungiValutazione(5)
-film_1.aggiungiValutazione(1)
-
-film_1.getMedia()
-film_1.getRate()
-
-film_1.recensione()
-
-
-
-
-
-    
 
