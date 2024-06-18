5eceb10d1ec7e3289be3229d7ea1b1adbedb4b14
danielinoo <daniele.pietropaolo1234@gmail.com>
2024-06-18 10:26:49 +0200

    .

--- lezione17/tests/test_persona.py
+++ lezione17/tests/test_persona.py
@@ -126,9 +126,16 @@ class TestFattura(unittest.TestCase):
 
         self.fattura_1.addPatient(self.paziente_2)
 
-        self.assertEqual(len(self.fattura_1.pazienti),1)
+        self.assertEqual(len(self.fattura_1.pazienti),2)
 
         self.fattura_1.getFatture(self)
+        self.fattura_1.getSalary(self)
+
+        self.assertNotEqual(self.fattura_1.salary,0)
+        self.assertNotEqual(self.fattura_1.fatture,0)
+
+        self.fattura_1.removePatient(self.paziente_1)
+        self.assertEqual(len(len(self.fattura_1.pazienti),1))
 
 
 
