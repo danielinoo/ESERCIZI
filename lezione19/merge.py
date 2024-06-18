285bc431b6b689bdc27e2a19f177a6b86e80ce27
danielinoo <daniele.pietropaolo1234@gmail.com>
2024-06-18 13:02:19 +0200

    merge

--- lezione19/merge.py
+++ lezione19/merge.py
@@ -1,56 +1,22 @@
 #Daniele Pietropaolo
 
 
-
-def merge(a,i1,f1,f2):
-
-    x = f2 - i1 +1
-
-    i = 1
-    i2 = f1 +1
-
-    while (i1 <= f1 and i2 <= f2):
-
-        if a[i1] <= a[i2]:
-            x[i] = a[i1]
-
-            i,i1 += 1
-
-        else:
-            x[i] = a[i2]
-
-            i,i2 += 1
-
-    if i1 < f1:
-
-        a[i2] = f1 
+def mergeSort(input_list : list[list]):
 
 
 
+    #punto medio
+    mid_point : int  = len(input_list) // 2
 
 
+    mergeSort(input_list = input_list[0:mid_point]) #prende prima meta dellla lista
+    mergeSort(input_list = input_list[mid_point :]) #prende seconda meta
 
 
+if __name__ == "__main__":
 
 
+    input_list : list[int] = [0,1,2,3,4,5,6,7]
 
 
-
-
-
-
-def mergeSort(a : list[int],i : int,f : int):
-
-    if i >= f:
-
-        return i
-    
-    m = (i + f) / 2
-
-    merge(a,i,m)
-    merge(a, m + 1,f)
-    merge(a,i,f,m)
-
-
-print(mergeSort(a = [1,2,3,4,5,6],i = 1,f = 2))
-
+    mergeSort( input_list = input_list)
