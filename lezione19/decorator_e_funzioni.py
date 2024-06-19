cffbb3a645adf20e9696d3f7119bb77071a1efc4
danielinoo <daniele.pietropaolo1234@gmail.com>
2024-06-19 18:14:46 +0200

    .

--- lezione19/decorator_e_funzioni.py
+++ lezione19/decorator_e_funzioni.py
@@ -1,38 +1,102 @@
-name = "marco"
+# name = "marco"
 
-def ciao(name : str):
+# def ciao(name : str):
 
-    return f"ciao, {name}"
+#     return f"ciao, {name}"
     
-def saluta_bob(func = ciao):
+# def saluta_bob(func = ciao):
 
-    return func("bob")
+#     return func("bob")
 
 
-def salve(name : str):
+# def salve(name : str):
 
-    return f"salve, {name}"
+#     return f"salve, {name}"
     
-print(saluta_bob(ciao))
-print(saluta_bob(salve))
+# print(saluta_bob(ciao))
+# print(saluta_bob(salve))
 
-##################################
+# ##################################
 
-#funzioni dentro funzioni (per funzionare la funzione principale (parent)deve essere richiamata)
+# #funzioni dentro funzioni (per funzionare la funzione principale (parent)deve essere richiamata)
 
-def parent():
+# def parent():
 
-    print("sono in funzione parent")
+#     print("sono in funzione parent")
 
-    def firts_child():
+#     def firts_child():
 
-        print("sono dentro funzione 1")
+#         print("sono dentro funzione 1")
 
-    def second_child():
+#     def second_child():
 
-        print("sono in funzione 2")
+#         print("sono in funzione 2")
 
-    firts_child()
-    second_child()
+#     firts_child()
+#     second_child()
 
-parent() #richiamo la funzione parent
+# parent() #richiamo la funzione parent
+
+
+
+"""decorators"""
+
+#decorator-> serve per modificare la funzione applicando un wrapper
+def decorator(func):
+
+    def wrapper():  #aggiunge due print alla funzione ciao
+        print("prima di func")
+
+        func()
+
+        print("dopo func")
+
+    return wrapper
+
+def ciao():
+    print("ciaooo")
+
+#aggiorna la funzione ciao
+ciao = decorator(ciao)
+
+ciao()
+
+print("\n ---------------------------------------\n")
+################
+
+"""esempio decorator:"""
+
+
+def decorator(func):
+
+    def wrapper():  #modifica la funzione ciao
+       import time
+
+       start = time.time()
+ 
+       func()
+       
+       print(f"tempo: {time.time() - start}")
+       
+    return wrapper
+
+
+ciao = decorator(ciao)
+
+ciao()
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
