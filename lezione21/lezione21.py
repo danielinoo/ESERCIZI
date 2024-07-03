285168877f098ba733a671f40b41912a2d14d2d2
danielinoo <daniele.pietropaolo1234@gmail.com>
2024-07-03 18:32:19 +0200

    .

--- lezione21/lezione21.py
+++ lezione21/lezione21.py
@@ -61,47 +61,68 @@
 
 ################################
 
-import sys
+# import sys
 
-a = []
-b = a
-print(sys.getrefcount(a))
+# a = []
+# b = a
+# print(sys.getrefcount(a))
 
-print("\n--------------\n")
-
-
-
-threads : list = []
-
-import time
-def thread_funcion(name):
-
-    print(f"time - {time.time()}")
-    time.sleep(2)
-    print(f"time - {time.time()}")
-
-
-import threading
-
-for i in range(3):
-    x = threading.Thread(target=thread_funcion,args=(1, ))
-    threads.append(x)
-    x.start()
+# print("\n--------------\n")
 
 
 
-for t in threads:
+# threads : list = []
 
-    t.join()
+# import time
+# def thread_funcion(name):
+
+#     print(f"time - {time.time()}")
+#     time.sleep(2)
+#     print(f"time - {time.time()}")
+
+
+# import threading
+# for i in range(3):
+#     x = threading.Thread(target=thread_funcion,args=(1, ))
+#     threads.append(x)
+#     x.start()
+
+
+
+# for t in threads:
+
+#     t.join()
 
     
-print("prima di thread")
-x.start()
-print(f"thread partito")
-x.join()
-print("thread finito?????")
+# print("prima di thread")
+# x.start()
+# print(f"thread partito")
+# x.join()
+# print("thread finito?????")
 
 
+#########################################################
 
+def funzione(id: int):
+    import time
+    import random
+    
+    sleep_time: int = random.randint(1, 10)
+    print(f"{id=} time {time.time()}")
+    time.sleep(sleep_time)
+    print(f"{id=} time {time.time()}")
+    
+if __name__ == "__main__":
+
+
+    import threading
+    from concurrent.futures import ThreadPoolExecutor
+    
+    with ThreadPoolExecutor(max_workers=10) as executor:
+    
+    
+#map -> mappa il primo elemento di range e lo usa come parametro nella "funzione" 
+#map -> puo processare 10 oggetti in parallelo (perche "max_workers = 10") 
+        executor.map(funzione, range(100))
 
 
---
+++ provadocker.py
@@ -0,0 +1,2 @@
+print("docker test           ")
+
