8d839efa660920ce033b0b3db7ef1679455f8de2
danielinoo <daniele.pietropaolo1234@gmail.com>
2024-09-06 11:42:30 +0200

    .

---
+++ python5/cercastringa/install.sh
@@ -0,0 +1,11 @@
+mkdir $home/cercastringa
+cp cerca.py $home/cercastringa
+cp requirements.txt $home/cercastringa
+cd cercastringa
+pip install virtualenv
+pip source myenv/bin/activate
+
+
+crea la cartella cercastringa con due file dentro (mkdir $home/cercastringa)
+copia nella cartella i due file
+crea il virtual enviroment
